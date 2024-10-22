e4faf549dc6cc32e5a6add9f5019b74d714fd649
Jose Augusto <jose.malengo@myp7.com.br>
2024-10-22 18:08:34 -0300

    add new background on HomeScreen

--- composeApp/src/commonMain/kotlin/org/example/project/homeApp/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/homeApp/HomeScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -24,7 +23,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.AlertDialog
 import androidx.compose.material.Card
 import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
@@ -40,6 +38,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
@@ -49,11 +48,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import coil3.compose.AsyncImage
 import firstkmp.composeapp.generated.resources.Res
+import firstkmp.composeapp.generated.resources.backgroundHome
 import firstkmp.composeapp.generated.resources.goals
-import org.example.project.AppRouterName
 import org.example.project.homeApp.components.cardLesson
 import org.example.project.sharedViewModel.SharedProfileViewModel
 import org.jetbrains.compose.resources.painterResource
@@ -81,21 +79,23 @@ fun HomeScreen(navToProfileScreen: () -> Unit, sharedProfileViewModel: SharedPro
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
+                .height(230.dp)
                 .clip(
                     RoundedCornerShape(bottomStart = 50.dp, bottomEnd = 50.dp)
                 )
-                .background(color = ColorsDefaults.primaryLight)
-
+                .paint(
+                    painterResource(resource = Res.drawable.backgroundHome),
+                    contentScale = ContentScale.FillBounds
+                )
         ) {
             Column (
                 modifier = Modifier.padding(top = 80.dp, start = 10.dp)
             ) {
                 Text(
                     text = ("Hello, "),
-                    fontStyle = FontStyle.Normal,
+                    fontStyle = FontStyle.Italic,
                     fontSize = 40.sp,
-                    color = ColorsDefaults.onPrimaryLight
+                    color = Color.Black
                 )
                 Text(
                     text = (sharedProfileViewModel.profile.value.displayName ?: ""),
